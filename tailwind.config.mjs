b6e4a3e556580b56c9eea685c6e3b5f1313f77ca
Alessandro Cipriani <social@magiaslab.com>
2024-03-21 18:47:38 +0100

    varie

---
+++ tailwind.config.mjs
@@ -0,0 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	theme: {
+		extend: {
+			colors: {
+			  primary: {"50":"#fefce8","100":"#fef9c3","200":"#fef08a","300":"#fde047","400":"#facc15","500":"#eab308","600":"#ca8a04","700":"#a16207","800":"#854d0e","900":"#713f12","950":"#422006"}
+			}
+		  },
+		  fontFamily: {
+			'body': [
+		  'Lato', 
+		  'ui-sans-serif', 
+		  'system-ui', 
+		  '-apple-system', 
+		  'system-ui', 
+		  'Segoe UI', 
+		  'Roboto', 
+		  'Helvetica Neue', 
+		  'Arial', 
+		  'Noto Sans', 
+		  'sans-serif', 
+		  'Apple Color Emoji', 
+		  'Segoe UI Emoji', 
+		  'Segoe UI Symbol', 
+		  'Noto Color Emoji'
+		],
+			'sans': [
+		  'Lato', 
+		  'ui-sans-serif', 
+		  'system-ui', 
+		  '-apple-system', 
+		  'system-ui', 
+		  'Segoe UI', 
+		  'Roboto', 
+		  'Helvetica Neue', 
+		  'Arial', 
+		  'Noto Sans', 
+		  'sans-serif', 
+		  'Apple Color Emoji', 
+		  'Segoe UI Emoji', 
+		  'Segoe UI Symbol', 
+		  'Noto Color Emoji'
+		]
+		  }
+		},
+	
+		
+	
+	plugins: [
+		require('flowbite/plugin'),
+		require('flowbite-typography')({
+			className: 'custom-class',
+		  }),
+	],
+}
+
