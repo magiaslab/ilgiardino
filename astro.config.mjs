eb3561841cdae0424f56ae0fd35aa00171bbcc3e
Alessandro Cipriani <social@magiaslab.com>
2024-04-14 08:54:04 +0200

    404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import lottie from "astro-integration-lottie";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ilgiardino.netlify.app',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), lottie()]
 });
