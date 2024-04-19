38595c7931b25c6c9a84044e1525d97a5f1729c2
Alessandro Cipriani <social@magiaslab.com>
2024-04-19 10:55:18 +0200

    collection

---
+++ src/content/camere/camera_menta.md
@@ -0,0 +1,22 @@
+---
+title: Camera Menta
+tipologia: Camera Matrimoniale
+description: Camera matrimoniale per due ospiti adulti, bagno con doccia, tv sat, WiFi free e tutto ciò che serve per un soggiorno perfetto
+background:
+  src: /img/DSC_4143.jpg
+  alt: Foto camera manta
+image: 
+    src: src/assets/menta/DSC_4129.jpg
+    alt: Foto camera manta
+imag1: 
+    src: src/assets/menta/DSC_4129.jpg
+    alt: Foto camera manta
+image2: 
+    src: src/assets/menta/DSC_4129.jpg
+    alt: Foto camera manta
+image3: 
+    src: src/assets/menta/DSC_4129.jpg
+    alt: Foto camera manta
+tags: camere
+
+---
---
+++ src/content/config.ts
@@ -0,0 +1,39 @@
+import { z, defineCollection, reference } from 'astro:content';
+
+
+const camereCollection = defineCollection({ 
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      tipologia: z.string(),
+      image: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+      imag1: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+      image2: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+      image3: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+      background: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+     
+    })
+   });
+
+
+
+   export const collections = {
+    'camere': camereCollection
+
+  };
