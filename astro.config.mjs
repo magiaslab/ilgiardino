6425ac5334891f2a7ba9b868040c25dc4a375a9a
Alessandro Cipriani <social@magiaslab.com>
2024-04-27 20:14:11 +0200

    prova

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
+
 export default defineConfig({
   site: 'https://ilgiardino.netlify.app',
   integrations: [tailwind(), sitemap(), lottie(), icon(), react()]
