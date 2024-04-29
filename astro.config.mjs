da6404d02ccb79551f41fd49d5ace2686485d9df
Alessandro Cipriani <social@magiaslab.com>
2024-04-29 10:37:14 +0200

    robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,34 @@ import lottie from "astro-integration-lottie";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
 
-
+// https://astro.build/config
 export default defineConfig({
   site: 'https://ilgiardino.netlify.app',
-  integrations: [tailwind(), sitemap(), lottie(), icon(), react()]
+  integrations: [tailwind(), sitemap(), lottie(), icon(), react(), robotsTxt(
+    {
+      policy: [
+        {
+          userAgent: 'Googlebot',
+          allow: '/',
+          disallow: ['/search'],
+          crawlDelay: 2,
+        },
+        {
+          userAgent: 'OtherBot',
+          allow: ['/allow-for-all-bots', '/allow-only-for-other-bot'],
+          disallow: ['/admin', '/login'],
+          crawlDelay: 2,
+        },
+        {
+          userAgent: '*',
+          allow: '/',
+          disallow: '/search',
+          crawlDelay: 10,
+          cleanParam: 'ref /articles/',
+        },
+      ],
+    }
+  )]
 });
